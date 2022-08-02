70983ebb9dccc6578df85077fb70da2fab7940d1
Lete114 <me@imlete.cn>
2022-08-02 20:44:54 +0800

    fix: searchParams 的 pahe 不合法产生的 bug

--- utils/searchParams.js
+++ utils/searchParams.js
@@ -3,6 +3,10 @@ module.exports = searchParams
 const baseURL = 'http://127.0.0.1'
 
 function searchParams(path) {
-  const url = new URL(path, baseURL)
-  return url.search + url.hash
+  try {
+    const url = new URL(path, baseURL)
+    return url.search + url.hash
+  } catch (error) {
+    return
+  }
 }
